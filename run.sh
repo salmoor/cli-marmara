222efe3d901eff29b3ad0d2a68f236ba187b45ec
Alemdar Salmoor <alemdarsalmoor@gmail.com>
2020-07-08 17:56:36 +0300

    Display today's date

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ u_menu=u_menu
 t_menu=t_menu
 f_menu=f_menu
 
+menu_offset=8
+day_of_week=$(($(date +%u) - 1))
+
+echo $day_of_week
+
 wget -q -O $u_menu http://kafemud.bilkent.edu.tr/kumanya_menusu.pdf
 pdftotext $u_menu $t_menu
 cut -d '/' -f 2 -s $t_menu | grep -v Lunch | grep -v '^$' | sed 's/^[ \t]*//' > $f_menu
