044fe4dcc95a41270600c88d953c9a4f4e1b9f96
Alemdar Salmoor <alemdarsalmoor@gmail.com>
2020-07-08 17:44:54 +0300

    Use variables

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-wget -q -O u_menu http://kafemud.bilkent.edu.tr/kumanya_menusu.pdf
-pdftotext u_menu t_menu
-cut -d '/' -f 2 -s t_menu | grep -v Lunch | grep -v '^$' | sed 's/^[ \t]*//' > f_menu
+u_menu=u_menu
+t_menu=t_menu
+f_menu=f_menu
+
+wget -q -O $u_menu http://kafemud.bilkent.edu.tr/kumanya_menusu.pdf
+pdftotext $u_menu $t_menu
+cut -d '/' -f 2 -s $t_menu | grep -v Lunch | grep -v '^$' | sed 's/^[ \t]*//' > $f_menu
